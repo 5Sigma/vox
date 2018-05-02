102d879c2ca5c4ffa62eab02421d593f8d39b673
Joe Bellus <joe@5sigma.io>
2018-05-02 17:51:49 -0400

    Removed assertion library
    Reverted all tests to use native testing package.

--- vox_test.go
+++ vox_test.go
@@ -3,17 +3,23 @@ package vox
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 var out = new(bytes.Buffer)
+var in *os.File
 
 func TestMain(m *testing.M) {
+	var err error
+	in, err = ioutil.TempFile("", "")
+	if err != nil {
+		panic(err.Error())
+	}
 	SetOutput(out)
+	SetInput(in)
 	res := m.Run()
 	os.Exit(res)
 }
@@ -21,19 +27,25 @@ func TestMain(m *testing.M) {
 func TestOutput(t *testing.T) {
 	Output("test")
 	res, _ := out.ReadString('\n')
-	assert.Equal(t, "test", res)
+	if res != "test" {
+		t.Errorf("Result not correct: %s", res)
+	}
 }
 
 func TestPrint(t *testing.T) {
 	Print("OUTPUT HERE")
 	res, _ := out.ReadString('\n')
-	assert.Equal(t, "OUTPUT HERE", res)
+	if res != "OUTPUT HERE" {
+		t.Errorf("Result not correct: %s", res)
+	}
 }
 
 func TestPrintln(t *testing.T) {
 	Println("OUTPUT HERE")
 	res, _ := out.ReadString('\n')
-	assert.Equal(t, "OUTPUT HERE", res)
+	if res != "OUTPUT HERE\n" {
+		t.Errorf("Result not correct: %s", res)
+	}
 }
 
 func TestPrintProperty(t *testing.T) {
@@ -42,14 +54,19 @@ func TestPrintProperty(t *testing.T) {
 	numSpaces := 60 - (len("Testing") + len("Run test"))
 	expected := fmt.Sprint(Yellow, "Testing",
 		strings.Repeat(" ", numSpaces), White,
-		"Run test", ResetColor)
-	assert.Equal(t, expected, res)
+		"Run test", ResetColor, "\n")
+
+	if res != expected {
+		t.Errorf("Result not correct: %s\n Expected: %s", res, expected)
+	}
 }
 
 func TestSprintc(t *testing.T) {
 	res := Sprintc(Red, "red")
 	expected := fmt.Sprint(Red, "red", ResetColor)
-	assert.Equal(t, expected, res)
+	if res != expected {
+		t.Errorf("Result not correct: %s\n Expected: %s", res, expected)
+	}
 }
 
 func TestPrintResult(t *testing.T) {
@@ -61,6 +78,8 @@ func TestPrintResult(t *testing.T) {
 		White, desc,
 		Yellow, "[",
 		Green, "OK",
-		Yellow, "]", ResetColor)
-	assert.Equal(t, expected, res)
+		Yellow, "]", ResetColor, "\n")
+	if res != expected {
+		t.Errorf("Result not correct: %s\n Expected: %s", res, expected)
+	}
 }
